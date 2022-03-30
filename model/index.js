31f80f3d49ce01e89fa0252c5c0eccf28f037b9d
NGGKI <kien.dv2012@gmail.com>
2022-03-30 13:51:33 -0700

    push

--- model/index.js
+++ model/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 
 const Employee = require('./employee')
-const Diagnose = require('./diagnose')
-const MedicalHistory = require('./medicalhistory')
 
-module.exports = { Employee, Patient }
+
+module.exports = { Employee }
